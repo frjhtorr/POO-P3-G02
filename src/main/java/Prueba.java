8341d0e3348561efcb20695020a61b10bfe94add
frjhtorr <frjhtorr@espol.edu.ec>
2023-11-10 21:29:42 -0500

    Prueba de repositorio

---
+++ src/main/java/Prueba.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Josh
+ */
+public class Prueba {
+    public void probar(){
+    System.out.println("Esto es un mensaje para ver cómo funciona el repositorio");
+    }
+}
