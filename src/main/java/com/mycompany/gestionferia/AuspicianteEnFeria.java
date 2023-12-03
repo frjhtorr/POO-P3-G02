585b182234e3894a401a32c9bf1680e6a36d5205
arcagarz <149555826+arcagarz@users.noreply.github.com>
2023-12-03 11:17:42 -0500

    Update AuspicianteEnFeria.java
    Actualizacion de auspiciante en feria

--- src/main/java/com/mycompany/gestionferia/AuspicianteEnFeria.java
+++ src/main/java/com/mycompany/gestionferia/AuspicianteEnFeria.java
@@ -2,47 +2,45 @@
 package com.mycompany.gestionferia;
 
 //IMPORTS
-import modelo.Auspiciante;
-    
+
 //CLASE
 public class AuspicianteEnFeria {
     //VARIABLES DE INSTANCIA
-    private Auspiciante aupiciante;
+    private Auspiciante auspiciante;
     private String auspicio;
-    private boolean tieneStand;
 
     //CONSTRUCTOR
-    public AuspicianteEnFeria(Auspiciante aupiciante, String auspicio, boolean tieneStand) {
-        this.aupiciante = aupiciante;
-        this.auspicio = auspicio;
-        this.tieneStand = tieneStand;
+    public AuspicianteEnFeria() {
     }
-    
-    //getters and setters
+    public AuspicianteEnFeria(Auspiciante auspiciante) {
+        this.auspiciante = auspiciante;
+    }
+    public AuspicianteEnFeria(Auspiciante auspiciante, String auspicio){
+      this.auspiciante = auspiciante;
+      this.auspicio = auspicio;
+    }
 
-    public Auspiciante getAupiciante() {
-        return aupiciante;
+    //getters and setters
+    
+    public Auspiciante getAuspiciante() {
+        return auspiciante;
     }
 
     public String getAuspicio() {
         return auspicio;
     }
 
-    public boolean isTieneStand() {
-        return tieneStand;
-    }
 
-    public void setAupiciante(Auspiciante aupiciante) {
-        this.aupiciante = aupiciante;
+    public void setAuspiciante(Auspiciante auspiciante) {
+        this.auspiciante = auspiciante;
     }
 
     public void setAuspicio(String auspicio) {
         this.auspicio = auspicio;
     }
 
-    public void setTieneStand(boolean tieneStand) {
-        this.tieneStand = tieneStand;
-    }
+
+}
     
     
     
