9da99d22dffb3685fdd5e0aebe72498ff92c8bf1
arcagarz <149555826+arcagarz@users.noreply.github.com>
2023-12-03 11:08:50 -0500

    Update Feria.java
    actualizacion parcial

--- src/main/java/com/mycompany/gestionferia/Feria.java
+++ src/main/java/com/mycompany/gestionferia/Feria.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.gestionferia;
-
-/**
- *
- * @author Josh
- */
+//IMPORTS
 import java.util.*;
+import java.time.LocalDate;
+import java.time.Month;
 
 public class Feria{
+  //VARIABLES DE INSTANCIA
     private String nombre;
     private String descripcion;
     private String fechaIni;
@@ -18,103 +12,135 @@ public class Feria{
     private String lugar;
     private String horario;
     private String codFeria;
-    private ArrayList<Emprededores> lstEmprendedores;
-    /*private ArrayList<AuspicianteEnFeria> auspiciantes;
-  private Seccion[] secciones;
-  private ArrayList<Emprendedor> listEmprendores;
-*/
+    private ArrayList<Emprendedor> lstEmprendedores;
+    private ArrayList<AuspicianteEnFeria> lstAuspiciantes;
+    private Seccion[] secciones = new Seccion[4];
 
-    public String getCodFeria() {
-        return codFeria;
-    }
-
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public String getFechaIni() {
-        return fechaIni;
-    }
-
-    public void setFechaIni(String fechaIni) {
-        this.fechaIni = fechaIni;
-    }
-
-    public String getFechaFin() {
-        return fechaFin;
-    }
-
-    public void setFechaFin(String fechaFin) {
-        this.fechaFin = fechaFin;
-    }
-
-    public String getLugar() {
-        return lugar;
-    }
-
-    public void setLugar(String lugar) {
-        this.lugar = lugar;
-    }
-
-    public String getHorario() {
-        return horario;
-    }
-
-    public void setHorario(String horario) {
-        this.horario = horario;
-    }
-
-  
-  
-  public Feria( String codFeria, String nombre, String descripcion, String lugar, String fechaIni, String fechaFin, String horario){
-    this.codFeria=codFeria;
-    this.nombre = nombre;
-    this.descripcion = descripcion;
-    this.fechaIni = fechaIni;
-    this.fechaFin = fechaFin;
-    this.lugar = lugar;
-    this.horario = horario;
-    //this.auspiciantes = auspiciantes;
-  }
-   
-  
-  
-  
-  public String toString(){//verFeria()
-    return "Hola";
-  }
-
-
-  /*public void asignarAuspiciante(Auspiciante a){
+  //CONSTRUCTORES
+    public Feria(){
     
-  }*/
+    }
+  
+    public Feria( String codFeria, String nombre, String descripcion, String lugar, String fechaIni, String fechaFin, String horario){
+      this.codFeria=codFeria;
+      this.nombre = nombre;
+      this.descripcion = descripcion;
+      this.fechaIni = fechaIni;
+      this.fechaFin = fechaFin;
+      this.lugar = lugar;
+      this.horario = horario;
+    }
 
- /* public ArrayList<Emprendedor> consultarEmprendedores(){
-    ArrayList<Emprendedor> emprendedores = new ArrayList<>();
-    return emprendedores;
-  }*/
-
-  public void mostrarDistribucion(){
-        
+//METODOS ------------------------------------------------
+  //GETTES AND SETTERS
+  public String getCodFeria() {
+      return codFeria;
   }
 
-  public void reservarStand(){
-    
+  public String getNombre() {
+      return nombre;
+  }
+
+  public void setNombre(String nombre) {
+      this.nombre = nombre;
+  }
+
+  public String getDescripcion() {
+      return descripcion;
+  }
+
+  public void setDescripcion(String descripcion) {
+      this.descripcion = descripcion;
+  }
+
+  public String getFechaIni() {
+      return fechaIni;
+  }
+
+  public void setFechaIni(String fechaIni) {
+      this.fechaIni = fechaIni;
+  }
+
+  public String getFechaFin() {
+      return fechaFin;
+  }
+
+  public void setFechaFin(String fechaFin) {
+      this.fechaFin = fechaFin;
+  }
+
+  public String getLugar() {
+      return lugar;
+  }
+
+  public void setLugar(String lugar) {
+      this.lugar = lugar;
+  }
+
+  public String getHorario() {
+      return horario;
+  }
+
+  public void setHorario(String horario) {
+      this.horario = horario;
+  }
+
+  public ArrayList<Emprendedor> getLstEmprendedores() {
+      return lstEmprendedores;
+  }
+
+  public void setLstEmprendedores(ArrayList<Emprendedor> lstEmprendedores) {
+      this.lstEmprendedores = lstEmprendedores;
+  }
+
+  public ArrayList<AuspicianteEnFeria> getLstAuspiciantes() {
+      return this.lstAuspiciantes;
+  }
+
+  public void setLstAuspiciantes(ArrayList<AuspicianteEnFeria> lstAuspiciantes) {
+      this.lstAuspiciantes = lstAuspiciantes;
+  }
+  public Seccion[] getSecciones(){
+    return secciones;
+  }
+  public void setSecciones(Seccion[] secciones){
+    this.secciones = secciones;
   }
   
-  
-} 
+//toString-----------------------------------------------
+  public String toString(){
+    String m = "";
+    for(int j =0; j<4; j++){
+       Seccion s = new Seccion();
+      //Id = Letra, Ej: A
+      String letra = s.getId();
+      int z = cantidadAus();
+      ArrayList<Stand> stands = new ArrayList<>(z);
+      stands = s.getListStands(); 
+
+      for(int k = 0; k<stands.size() ; k++){//ITERA PARA MOSTAR LOS STANDS EN LAS SECCIONES
+        Stand st = new Stand();
+        int i=st.getNum();
+        System.out.println("Seccion"+ i);
+        if(st.getReservado()){
+        m += "["+letra+i+"*]";
+      }else{
+        m += "["+letra+i+"]";
+        } 
+      }
+      m += "\n";
+    }
+    return "-------------------------"+
+      "\n Código: " + codFeria +
+      "\n Nombre de la Feria: " + nombre +
+      "\n Lugar: " + lugar +
+      "\n Fecha de incio: " + fechaIni +
+      "\n Fecha de fin: " + fechaFin +
+      "\n Horario: " + horario +
+      "\n Auspiciantes: " + lstAuspiciantes +
+      "\n Distribucion: "+"\n"+m;
+
+
+  }
+
 
